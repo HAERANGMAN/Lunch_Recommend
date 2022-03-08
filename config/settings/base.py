a00e7a306cc24389f7a893706187e615a3ffe01b
HAERANG <abcrang@gmail.com>
2022-03-08 17:50:15 +0900

    setting last

--- config/settings/base.py
+++ config/settings/base.py
@@ -25,8 +25,8 @@ SECRET_KEY = 'u=q#u^yp62+7c__0yadnnuh9$(ia)q0_mb8j32l-v3)w-ops8f'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-#ALLOWED_HOSTS = ['15.164.254.209']
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['15.164.254.209']
+#ALLOWED_HOSTS = []
 
 # Application definition
 
